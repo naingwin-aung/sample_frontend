494cec477b0a69e9d357a046fd5548566f7f28e6
NaingWinAung <naingwinaung1710@gmail.com>
2025-12-03 01:27:59 +0700

    product detail page

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -13,8 +13,32 @@ const ProductDetail = () => {
   }, [param.slug]);
 
   return (
-    <Container className="mt-5">
-      {detail?.title}
+    <Container className="mt-6">
+      <h2 className="text-3xl font-medium mb-6">
+        {detail?.title}
+      </h2>
+      {/* gallery here */}
+      <div className="w-full h-[362px] border border-gray-200 rounded-2xl mb-6">
+      </div>
+
+      {/* description */}
+      <div className="flex gap-4 mb-6">
+        <div className="w-2/3 h-[170px] border border-gray-200 rounded-2xl">
+
+        </div>
+        <div className="w-1/3 h-[120px] border border-gray-200 rounded-2xl">
+        </div>
+      </div>
+
+      {/* select option */}
+      <div>
+        <h3 className="text-2xl font-medium mb-4">Select options</h3>
+        <div className="flex flex-col gap-4">
+          <div className="w-full h-[190px] border border-gray-200 rounded-2xl"></div>
+          <div className="w-full h-[190px] border border-gray-200 rounded-2xl"></div>
+          <div className="w-full h-[190px] border border-gray-200 rounded-2xl"></div>
+        </div>
+      </div>
     </Container>
   );
 };
