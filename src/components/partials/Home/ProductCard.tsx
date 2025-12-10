c18ccab6fb043690a7775a46387a884be3a6040d
NaingWinAung <naingwinaung1710@gmail.com>
2025-12-10 16:34:02 +0700

    product card

--- src/components/partials/Home/ProductCard.tsx
+++ src/components/partials/Home/ProductCard.tsx
@@ -10,7 +10,7 @@ const ProductCard = ({ product }: { product: ProductType }) => {
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="w-full h-[390px] border border-gray-200 rounded-2xl cursor-pointer hover:shadow">
+      <div className="w-full h-[393px] border border-gray-200 rounded-2xl cursor-pointer hover:shadow">
         <div className="w-full h-[200px]">
           <Image
             src={product?.images[0]?.url}
@@ -19,26 +19,27 @@ const ProductCard = ({ product }: { product: ProductType }) => {
           />
         </div>
         <div className="p-4 h-[150px]">
-          <div className="text-sm text-gray-500 mb-1 flex items-center">
+          <div className="text-xs text-gray-500 mb-1 flex items-center">
             {product.product_type}
             <div>
               <Dot size={16} />
             </div>
             {product.city}
           </div>
-          <h3 className="text-md font-medium mb-2 line-clamp-2">
+          <h3 className="text-md font-medium mb-1 line-clamp-2" title={product.name}>
             {product.name}
           </h3>
-          <div className="text-[11px] text-gray-400 line-clamp-4 p-1 rounded bg-gray-100 w-max">
+          <div className="text-[11px] line-clamp-4">
             {product.piers.length > 0 &&
-              `${product.piers
-                .slice(0, 2)
-                .map((pier) => pier.name)
-                .join(", ")}`}
+              product.piers.slice(0, 2).map((pier) => (
+                <span key={pier.id} className="mr-1 text-primary bg-orange-100 p-1 rounded-md">
+                  {pier.name}
+                </span>
+              ))}
           </div>
         </div>
         {/* footer */}
-        <div className="flex justify-between items-center px-4 pb-4">
+        <div className="flex justify-between items-center px-4">
           <span className="text-sm text-gray-500"></span>
           <span className="text-md font-medium">
             From THB {product.min_price}
