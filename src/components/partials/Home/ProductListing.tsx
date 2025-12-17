5bc02d3583f4192ff0c8daee1988176eb126ff4e
NaingWinAung <naingwinaung1710@gmail.com>
2025-12-17 12:20:36 +0700

    product lsiing

--- src/components/partials/Home/ProductListing.tsx
+++ src/components/partials/Home/ProductListing.tsx
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import type { ProductType } from "../../../types/ProductType";
 import ProductCard from "./ProductCard";
 import { ListProductQueryOption } from "../../../api/Product/products";
-import Container from "../../global/Container";
 
 const ProductListing = () => {
   const { data } = useQuery({
@@ -10,14 +9,11 @@ const ProductListing = () => {
   });
 
   return (
-    <Container>
-    <h2 className="text-2xl mb-7">Explore the World Through Unforgettable Cruises</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
-        {data?.data.map((product: ProductType) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
-    </Container>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
+      {data?.data.map((product: ProductType) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,22 @@
-import Banner from "../components/partials/Home/Banner";
+// import Banner from "../components/partials/Home/Banner";
+import Container from "../components/global/Container";
 import ProductListing from "../components/partials/Home/ProductListing";
 
 const Home = () => {
   return (
     <section className="flex flex-col gap-12">
-      <div className="mb-40 md:mb-15">
+      {/* <div className="mb-40 md:mb-15">
         <Banner />
-      </div>
-      <div className="mt-5">
+      </div> */}
+      <Container className="mt-7">
+        <h2 className="text-2xl mb-6">
+          Explore the World Through Unforgettable Cruises
+        </h2>
+        {/* <div className="flex justify-between items-center mb-4">
+          <h4 className="">10K+ cruises</h4>
+        </div> */}
         <ProductListing />
-      </div>
+      </Container>
     </section>
   );
 };
